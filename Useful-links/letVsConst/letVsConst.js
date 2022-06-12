4f30051a6d9bc299cdbeb0f9f3e32a91df48cef1
Julian <julian.martinez.cs@gmail.com>
2022-06-12 21:10:51 +0100

    Started a lilttle webside with useful links

---
+++ Useful-links/letVsConst/letVsConst.js
@@ -0,0 +1,11 @@
+function myFunction1() {
+    window.location.href="https://wesbos.com/let-vs-const";
+  }
+
+function myFunction2() {
+    window.location.href="https://4geeks.com/syllabus/europe-prework-fs/read/what-is-javascript-learn-to-code-in-javascript";
+  }
+
+function myFunction3() {
+    window.location.href="https://wesbos.com/let-vs-const";
+  }
